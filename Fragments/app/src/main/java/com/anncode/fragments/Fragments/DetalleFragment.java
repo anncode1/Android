add1fc0c3edb4026fcd0a3457f466df56a4f392b
Ann <anahi.anndroid@gmail.com>
2016-06-18 18:55:10 -0500

    Fragments Clase 1

---
+++ Fragments/app/src/main/java/com/anncode/fragments/Fragments/DetalleFragment.java
@@ -0,0 +1,31 @@
+package com.anncode.fragments.Fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.anncode.fragments.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class DetalleFragment extends Fragment {
+
+
+    public DetalleFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_detalle, container, false);
+    }
+
+}
