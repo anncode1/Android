fcb9b8c390ede5b48cfff1aa6137f1e988528771
Ann <anahi.anndroid@gmail.com>
2016-06-18 18:59:45 -0500

    Fragment Clase 1 POJO Amigo

---
+++ Fragments/app/src/main/java/com/anncode/fragments/model/Amigo.java
@@ -0,0 +1,45 @@
+package com.anncode.fragments.model;
+
+/**
+ * Created by anahisalgado on 18/06/16.
+ */
+public class Amigo {
+
+    private String nombre;
+    private String twitter;
+    private String telefono;
+
+    public Amigo() {
+    }
+
+    public Amigo(String nombre, String twitter, String telefono) {
+        this.nombre = nombre;
+        this.twitter = twitter;
+        this.telefono = telefono;
+    }
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTwitter() {
+        return twitter;
+    }
+
+    public void setTwitter(String twitter) {
+        this.twitter = twitter;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+}
